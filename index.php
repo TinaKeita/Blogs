9997d7ad48ae61f52b0d5e8988d543e6073dc5cf
ipb23.t.paegle <ipb23.t.paegle@pjvt.local>
2024-12-10 15:05:57 +0200

    Izvadīti html formātā visi posts no datubāzes.

--- index.php
+++ index.php
@@ -1,8 +1,17 @@
 <?php
+require "functions.php";
 
-echo "Iet";
+echo "Iet<br>
+<style>
+body {
+    background-color:D7D3BF;
+    font-size: 20px;}
+
+</style>
+        ";
 // 1. Izveidot datu bazi ar tabulu✅
 // 2. Savienot PHP ar datu bāzi
+// 3. Izvadit datus ka HTML
 
 // db nosaukums, parole, lietotajvards
 // mysql_ connect❌
@@ -12,4 +21,20 @@ $dsn = "mysql:host=localhost;port=3306;user=root;password=;dbname=blog_ipb23;cha
 
 // PDO -PHP Data Object
 $pdo = new PDO($dsn);
+
+// 1. sagatavot vaicājumu (statement)
+$statement = $pdo->prepare("SELECT * FROM posts");
+// 2.Izpildit statement
+$statement->execute();
+// 3. Dabut rezultatu
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+//dd($posts[0] ["content"]);
+
+echo "<ul>";
+foreach ($posts as $post) {
+    echo "<li>" . $post["content"] . "</li>";
+}
+echo "</ul>";
+
 ?>
