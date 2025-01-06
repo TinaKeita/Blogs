60af0648b897fba01911090c3d75ca78fc80a252
ipb23.t.paegle <ipb23.t.paegle@pjvt.local>
2025-01-06 11:27:46 +0200

    Izveidots search bar

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ require "functions.php"; //savienojamies ar citiem failiem
 require "Database.php";
 $config = require("config.php");
 
-echo "Iet<br>
+echo "
 <style>
     body {
       background-color:D7D3BF;
@@ -13,11 +13,29 @@ echo "Iet<br>
 
 </style>";
 
+//Mērķis: uztaisīt filtru - ierakstu meklēšana
+//HTML forma, input un submit.
+//Kaut kā uzrakstīt PHP
+//Atgriezt datus no SQL
+
 $db = new Database($config["database"]); //jauna klase kas savieno ar datu bāzi
 // Pie new Database izpildās konstrukotrs
 $posts = $db->query("SELECT * FROM posts")->fetchAll(); //metode atgriež asoc masīvu ar datiem
 
 //dd($posts[0] ["content"]); - izsauc dump and die
+
+
+if  (isset($_GET["search_query"]) && $_GET["search_query"] != ""){
+  echo "Atgriezt ierakstus";
+  //TODO atrodi īstos ierakstus
+  $posts = $db->query("SELECT * FROM posts WHERE content LIKE '%" . $_GET["search_query"] . "%';")->fetchAll();
+}
+echo "<h2>Blogs</h2>";
+
+echo "<form>";
+echo "<input name='search_query' />";
+echo "<button>Meklēt</button>"; //search bars
+echo "</form>";
 
 echo "<ul>";
     foreach ($posts as $post) {
