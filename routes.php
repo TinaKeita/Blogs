198343bb755b31f85df8278b215249a9ce5adb2e
ipb23.t.paegle <ipb23.t.paegle@pjvt.local>
2025-01-14 15:14:46 +0200

    kks neiet

--- routes.php
+++ routes.php
@@ -4,7 +4,8 @@ return [
     "/" => "controllers/posts/index.php",
     "/about" => "controllers/story.php",
     "/show" => "controllers/posts/show.php",
-    "/categories" => "controllers/categories.php"
+    "/categories" => "controllers/categories.php",
+    "/create" => "contollers/posts/create.php"
 ]
 
 ?>
---
+++ views/posts/create.view.php
@@ -0,0 +1,9 @@
+<?php require "views/components/header.php"?>
+<?php require "views/components/navbar.php"?> 
+    
+<h2>Izveido bloga ierakstu!</h2>
+<form method="POST">
+<label><input name="content" /></label>
+
+<!-- php tagu count - 5 -->
+<?php require "views/components/footer.php"?>
