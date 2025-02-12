09a9dcc42172d9e146c538e920fcd401df4db636
rgulbis <rgubis41@gmail.com>
2025-02-12 11:10:44 +0200

    Aizmirsu parmainit atpakal paroli

--- config.php
+++ config.php
@@ -6,7 +6,7 @@ return[
         "host" => "localhost",
         "port" => "3306",
         "user" => "root",
-        "password" => "root",
+        "password" => "",
         "dbname" => "blog_ipb23",
         "charset" => "utf8mb4"
         ] //masīvs masīvā
